84bac72e15f13c22d660d1d87a5dd803a45ae168
AliceVoice <poctuk22@gmail.com>
2024-01-22 17:49:42 +0300

    Bugfix

--- src/main/java/pro/sky/calclulator/CalcController.java
+++ src/main/java/pro/sky/calclulator/CalcController.java
@@ -13,27 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
     }
 
     @GetMapping (path = "/calculator")
-    public String answerHello(){
-        return calcService.hello();
+    public String hello(){
+        return "Добро пожаловать в калькулятор";
     }
 
     @GetMapping("/calculator/plus")
     public String plus(@RequestParam(required = true) int num1, @RequestParam(required = true) int num2) {
-        return calcService.addOperation(num1, num2);
+        return num1 + " + " + num2 + " = " + calcService.addOperation(num1, num2);
     }
 
     @GetMapping("/calculator/minus")
     public String minus(@RequestParam(required = true) int num1, @RequestParam(required = true) int num2) {
-        return calcService.minusOperation(num1, num2);
+        return num1 + " - " + num2 + " = " + calcService.minusOperation(num1, num2);
     }
 
     @GetMapping("/calculator/multiply")
     public String multiply(@RequestParam(required = true) int num1, @RequestParam(required = true) int num2) {
-        return calcService.multiplyOperation(num1, num2);
+        return num1 + " * " + num2 + " = " + calcService.multiplyOperation(num1, num2);
     }
 
     @GetMapping("/calculator/divide")
     public String divide(@RequestParam(required = true) int num1, @RequestParam(required = true) int num2) {
-        return calcService.divideOperation(num1, num2);
+        return num1 + " / " + num2 + " = " + calcService.divideOperation(num1, num2);
     }
 }
--- src/main/java/pro/sky/calclulator/CalcService.java
+++ src/main/java/pro/sky/calclulator/CalcService.java
@@ -4,31 +4,28 @@ import org.springframework.stereotype.Service;
 
     @Service
 public class CalcService {
-    public String hello() {
-        return "Добро пожаловать в калькулятор";
-    }
 
-    public String addOperation(int num1, int num2) {
+    public int addOperation(int num1, int num2) {
         int total = num1 + num2;
-        return num1 + " + " + num2 + " = " + total;
+        return total;
     }
 
-    public String minusOperation(int num1, int num2) {
+    public int minusOperation(int num1, int num2) {
         int total = num1 - num2;
-        return num1 + " - " + num2 + " = " + total;
+        return total;
     }
 
-    public String multiplyOperation(int num1, int num2) {
+    public int multiplyOperation(int num1, int num2) {
         int total = num1 * num2;
-        return num1 + " * " + num2 + " = " + total;
+        return total;
     }
 
-    public String divideOperation(int num1, int num2) {
+    public int divideOperation(int num1, int num2) {
         if (num2 == 0) {
             throw new IllegalArgumentException("Ошибка: деление на ноль");
         }
         int total = num1 / num2;
-        return num1 + " / " + num2 + " = " + total;
+        return total;
     }
 
 }
