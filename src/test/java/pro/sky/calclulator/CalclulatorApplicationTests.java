4eca9b1a4b30c54092f48a460827e50a429f8e29
AliceVoice <poctuk22@gmail.com>
2024-01-22 11:08:39 +0300

    Initial commit

---
+++ src/test/java/pro/sky/calclulator/CalclulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.calclulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalclulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
